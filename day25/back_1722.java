8fecb33a6c8e37ef8f7fb18d65228a3828dcc00f
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-09-21 02:15:10 +0900

    0주차 과제 4.토마토(124176KB,568ms).java

--- day25/back_1722.java
+++ day25/back_1722.java
@@ -11,24 +11,55 @@ public class back_1722 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
-		int height=fac(N);
-
-		D= new long[height+1][N+1];
-		for(int i=1;i<=height;i++)
+		int F[] = new int[N+1];
+		int S[] = new int[N+1];
+		F[1]=1;
+		for(int i=2;i<=N;i++)
 		{
-
+			F[i] = F[i-1]*i;
 		}
 		int a = Integer.parseInt(st.nextToken());
 		int result[]= new int[N];
+		boolean visited[] = new boolean[N+1];
 		if(a==1)
 		{
-
-		}else{
-			st = new StringTokenizer(br.readLine());
-			for(int i=0;i<N;i++)
+			long k = Integer.parseInt(st.nextToken());
+			for(int i=1;i<=N;i++)
 			{
-				result[i] = Integer.parseInt(st.nextToken());
+				for(int j=1,cnt =1;j<=N;j++)
+				{
+					if(visited[j])
+						continue;
+					if(k<=cnt*F[N-1])
+					{
+						k-=((cnt-1)*F[N-1]);
+						S[i] = j;
+						visited[j] = true;
+						break;
+					}
+					cnt++;
+				}
 			}
+			for(int i=1;i<=N;i++)
+			{
+				System.out.println(S[i]+" ");
+			}
+		}else{
+			long k =1;
+
+			for(int i=1;i<=N;i++)
+			{	st = new StringTokenizer(br.readLine());
+				S[i] = Integer.parseInt(st.nextToken());
+				long cnt = 0;
+				for(int j=1;j<S[j];j++)
+				{
+					if(visited[j]==false) cnt++;
+				}
+				k+=cnt*F[N-1];
+				visited[S[i]]= true;
+			}
+			System.out.println(k);
+
 		}
 
 	}
