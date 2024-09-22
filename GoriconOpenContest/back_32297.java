9d86f1088bb58ba7d813e29a8b27e612958db1be
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-09-22 18:46:34 +0900

    GoriconOpenContest  A: (14252KB,100ms).java

---
+++ GoriconOpenContest/back_32297.java
@@ -0,0 +1,23 @@
+package GoriconOpenContest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//A
+public class back_32297 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine();
+		String s= br.readLine();
+		if(s.contains("gori"))
+		{
+			System.out.println("YES");
+		}
+		else
+		{
+			System.out.println("NO");
+		}
+
+	}
+}
