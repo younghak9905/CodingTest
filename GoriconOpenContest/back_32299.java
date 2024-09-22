9f5b804bc7458dc0ebb73da2d2fc9cdb78e5bf44
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-09-22 20:04:30 +0900

    GoriconOpenContest  C: (14260kb,96ms).java

---
+++ GoriconOpenContest/back_32299.java
@@ -0,0 +1,20 @@
+package GoriconOpenContest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class back_32299 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N!=3)
+		{
+			System.out.println("Sangho");
+		}
+		else
+		{
+			System.out.println("Hobanwoo");
+		}
+	}
+}
