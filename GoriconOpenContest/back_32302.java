b4fa59a397fc43b6f63d806046641f1893eee9bd
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-09-26 13:42:16 +0900

    goricon  F: (실패).java

---
+++ GoriconOpenContest/back_32302.java
@@ -0,0 +1,19 @@
+package GoriconOpenContest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class back_32302 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		for(int i=N-3;i>=0;i++)
+		{
+
+		}
+	}
+}
